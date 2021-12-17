b4f4ecd3e796dd09c16898102b479aa6eba8e777
Vollmers Lab <ig88@holowan.ucsc.edu>
2021-12-16 20:11:35 -0800

    added argparse

--- utils/removePolyA.py
+++ utils/removePolyA.py
@@ -1,5 +1,23 @@
 import sys
 import mappy
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--inFile', '-i', type=str, action='store', help='file to be adapter and poly trimmed')
+parser.add_argument('--outFile', '-o', type=str, action='store', help='output file')
+parser.add_argument('--trimmedBases', '-t', type=str, action='store', help='in the format [number,number] the number of bases that are going to be trimmed from 5prime and 3prime end (pre-polyA trimming)')
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(0)
+args = parser.parse_args()
+inFile = args.inFile
+outFile = args.outFile
+trimmedBases=args.trimmedBases
+
+
 
 def removePolyA(seq):
     reverse = seq[::-1]
@@ -34,11 +52,11 @@ def removePolyA(seq):
 def main():
     trim=False
     if len(sys.argv)>3:
-        trim=sys.argv[3].split(',')
+        trim=trimmedBases.split(',')
         fivePrime=int(trim[0])
         threePrime=-int(trim[1])
-    out=open(sys.argv[2],'w')
-    for name,seq,qual in mappy.fastx_read(sys.argv[1]):
+    out=open(outFile,'w')
+    for name,seq,qual in mappy.fastx_read(inFile):
         if trim:
             seq=seq[fivePrime:threePrime]
         seqTrim,Astate=removePolyA(seq)
