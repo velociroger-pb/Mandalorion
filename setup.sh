6c2bc512b8c0b91fc7c2e251774d5e795678a246
Vollmers Lab <ig88@holowan.ucsc.edu>
2023-06-21 16:58:45 -0700

    updated for v4.2

--- setup.sh
+++ setup.sh
@@ -5,17 +5,19 @@ cwd=$(pwd)
 
 # Resolve dependencies
 
-echo 'Pip installables (scipy, numpy, pyabpoa, mappy, Cython, tqdm)'
-python3 -m pip install --user --upgrade numpy pyabpoa==1.4.0 mappy Cython
-
-echo "emtrey"
-git clone https://github.com/rvolden/emtrey.git
-cd emtrey && make
-cd $cwd
+echo 'Pip installables (scipy, numpy, mappy, Cython)'
+python3 -m pip install --user --upgrade numpy mappy Cython
 
 echo "minimap2"
 git clone https://github.com/lh3/minimap2
 cd minimap2 && make
 cd $cwd
+
+echo 'abpoa'
+wget https://github.com/yangao07/abPOA/releases/download/v1.4.1/abPOA-v1.4.1.tar.gz
+tar -zxvf abPOA-v1.4.1.tar.gz
+cd abPOA-v1.4.1; make
+cd $cwd
+rm abPOA-v1.4.1.tar.gz
 
 echo 'Done'
